5a6ab1fb3a0662f1d5c0fdfc63f06b14be1e708b
Matthew-MT <thumpber540@gmail.com>
2022-09-20 09:12:42 -0600

    Everything runs now

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import "@cloudscape-design/global-styles/index.css";
+import Applayout from "@cloudscape-design/components/app-layout";
 import Input from "@cloudscape-design/components/button";
-import {useState} from 'react';
+import './App.css';
 
 function App() {
     const
         [value, setValue] = useState();
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <Input></Input>
-            </header>
-        </div>
+        <Applayout
+            content={
+                <Input
+                    type="text"
+                    onChange={event => setValue(event.detail.value)}
+                    value={value}
+                />
+            }
+        />
     );
 }
 
