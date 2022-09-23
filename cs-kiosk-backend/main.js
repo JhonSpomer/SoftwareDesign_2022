5604185d39c661fcced9e3571f0b094b0f4b2e96
Matthew-MT <thumpber540@gmail.com>
2022-09-22 19:31:43 -0600

    Added BrowserRouter component

--- cs-kiosk-backend/main.js
+++ cs-kiosk-backend/main.js
@@ -1,13 +1,15 @@
 const {app, BrowserWindow} = require("electron");
 
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") app.quit();
-});
-
-await app.whenReady();
-
-const win = new BrowserWindow({
-    fullscreen: true
-});
-
-win.loadFile("./public/index.html");
+;(async () => {
+    app.on("window-all-closed", () => {
+        if (process.platform !== "darwin") app.quit();
+    });
+    
+    await app.whenReady();
+    
+    const win = new BrowserWindow({
+        fullscreen: true
+    });
+    
+    win.loadFile("./public/index.html");
+})();
