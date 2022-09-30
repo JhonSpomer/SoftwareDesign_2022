cf6369cfbce618af689a095a83ef44d90abde4d6
Matthew-MT <thumpber540@gmail.com>
2022-09-29 19:49:36 -0600

    Working on carousel. Got carousel working in a basic sense.

--- cs-kiosk-backend/main.js
+++ cs-kiosk-backend/main.js
@@ -1,4 +1,5 @@
-const {app, BrowserWindow} = require("electron");
+const path = require("path");
+const {app, BrowserWindow, protocol} = require("electron");
 
 ;(async () => {
     app.on("window-all-closed", () => {
@@ -6,10 +7,15 @@ const {app, BrowserWindow} = require("electron");
     });
     
     await app.whenReady();
+
+    protocol.interceptFileProtocol('file', (request, callback) => {
+        const url = request.url;
+        if (url.includes(__dirname)) return callback(url.replace("file:///", ""));
+        callback(path.normalize(`${__dirname}/public/${url.slice(7)}`).replace("file:///", ""));
+    });
     
     const win = new BrowserWindow({
         fullscreen: true
     });
-    
-    win.loadFile("./public/index.html");
+    win.loadFile("public/index.html");
 })();
--- cs-kiosk-display/src/App.js
+++ cs-kiosk-display/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import Carousel from "react-bootstrap/Carousel";
 import './App.css';
+import logo from './logo.svg';
+import "./bootstrap.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const
+        [index, setIndex] = useState(0);
+
+    setInterval(() => setIndex((index + 1) % 2), 2000);
+
+    return <Carousel
+        controls={false}
+        indicators={false}
+        activeIndex={index}
+        onSelect={i => setIndex(i)}
+    >
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={logo}
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <p>
+                Hello world!
+            </p>
+        </Carousel.Item>
+    </Carousel>;
 }
-
-export default App;
