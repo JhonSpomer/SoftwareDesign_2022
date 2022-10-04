11ad3a7a31c480f3fc2d39942886cb9484960df8
Matthew-MT <thumpber540@gmail.com>
2022-10-04 14:51:04 -0600

    Finalising kiosk display deployment structure. Kiosk now works as intended while also supplying an endpoint for the admin dashboard to query a preview of the kiosk display.

--- cs-kiosk-backend/main.js
+++ cs-kiosk-backend/main.js
@@ -1,21 +1,35 @@
-const path = require("path");
-const {app, BrowserWindow, protocol} = require("electron");
+const
+    path = require("path"),
+    express = require("express"),
+    {app, BrowserWindow, protocol} = require("electron");
+
+const
+    port = 9000,
+    api = express();
 
 ;(async () => {
-    app.on("window-all-closed", () => {
-        if (process.platform !== "darwin") app.quit();
-    });
-    
     await app.whenReady();
 
-    protocol.interceptFileProtocol('file', (request, callback) => {
-        const url = request.url;
-        if (url.includes(__dirname)) return callback(url.replace("file:///", ""));
-        callback(path.normalize(`${__dirname}/public/${url.slice(7)}`).replace("file:///", ""));
+    // Express API server
+
+    api.use("/carousel", express.static(path.join(__dirname, "public/carousel")));
+
+    const server = api.listen(port, () => {
+        console.log("Server running on port", port);
     });
+
+    // Electron kiosk display
     
     const win = new BrowserWindow({
         fullscreen: true
     });
-    win.loadFile("public/index.html");
+
+    win.loadURL("http://localhost:9000/carousel");
+
+    // Cleanup
+
+    app.on("window-all-closed", () => {
+        if (process.platform !== "darwin") app.quit();
+        server.close();
+    });
 })();
--- cs-kiosk-display/src/App.js
+++ cs-kiosk-display/src/App.js
@@ -5,24 +5,22 @@ import logo from './logo.svg';
 import "./bootstrap.css";
 
 export default function App() {
-    const
-        [index, setIndex] = useState(0);
-
-    setInterval(() => setIndex((index + 1) % 2), 2000);
-
     return <Carousel
         controls={false}
         indicators={false}
-        activeIndex={index}
-        onSelect={i => setIndex(i)}
+        pause={false}
     >
-        <Carousel.Item>
+        <Carousel.Item
+            interval={500}
+        >
             <img
                 className="d-block w-100"
                 src={logo}
             />
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item
+            interval={500}
+        >
             <p>
                 Hello world!
             </p>
