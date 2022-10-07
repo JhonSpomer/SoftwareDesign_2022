a600c0721283a386f4d5e40514095f97273676fa
Matthew-MT <thumpber540@gmail.com>
2022-10-06 18:43:03 -0600

    Adding mongodb types

--- cs-kiosk-backend/main.js
+++ cs-kiosk-backend/main.js
@@ -1,11 +1,37 @@
 const
     path = require("path"),
     express = require("express"),
-    {app, BrowserWindow, protocol} = require("electron");
+    {app, BrowserWindow, protocol} = require("electron"),
+    fs = require("fs"),
+    Binary = require("mongodb").Binary;
 
 const
     port = 9000,
     api = express();
+
+const
+    dummySlides = [
+        {
+            id: "slide0",
+            type: "link",
+            content: "https://example.com"
+        },
+        {
+            id: "slide1",
+            type: "image",
+            content: Binary(fs.readFileSync("1.png"))
+        },
+        {
+            id: "slide2",
+            type: "link",
+            content: "https://example.com"
+        }
+    ],
+    dummyOrder = [
+        "slide0",
+        "slide1",
+        "slide2"
+    ];
 
 ;(async () => {
     await app.whenReady();
@@ -16,6 +42,26 @@ const
 
     const server = api.listen(port, () => {
         console.log("Server running on port", port);
+    });
+
+    api.get("/slides.json", (req, res) => {
+        res.status(200).send("Hello world!");
+    });
+
+    api.post("/slides.json", (req, res) => {
+        req.on("data", chunk => console.log(chunk.toString()));
+        req.on("close", () => console.log("Closed"));
+        res.status(200).send("Done");
+    });
+
+    api.get("/order.json", (req, res) => {
+        res.status(200).send(JSON.stringify(dummyOrder));
+    });
+
+    api.post("/order.json", (req, res) => {
+        req.on("data", chunk => console.log(chunk.toString()));
+        req.on("close", () => console.log("Closed"));
+        res.status(200).send("Done");
     });
 
     // Electron kiosk display
