8e64335015b0d85be40492a51939be5b625a2bac
Byte-By-Byte <00isaac.vh@gmail.com>
2022-10-06 19:50:27 -0600

    added new user function

--- cs-kiosk-backend/src/DB_functions.js
+++ cs-kiosk-backend/src/DB_functions.js
@@ -1,6 +1,24 @@
 import { MongoClient } from "mongodb";
+const fs = require('fs');
 
-function newSlide(params) {
-    
-    
-}
+const buffer = fs.readFileSync("../.mongodb.auth")
+const uri = buffer.toString();
+
+const client = new MongoClient(uri)
+
+async function newUser(UN, PS) {
+    try {
+      const database = client.db("BulletinDisplay");
+      const haiku = database.collection("users");
+      // create a document to insert
+      const doc = {
+        username: UN,
+        password: PS,
+      }
+      const result = await users.insertOne(doc);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
