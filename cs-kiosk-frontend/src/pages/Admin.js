1d4709cbacff85415aee53d1670767e24b272c37
Matthew-MT <thumpber540@gmail.com>
2022-10-11 09:14:32 -0600

    Working on an alternate way to display editable slides

--- cs-kiosk-frontend/src/pages/Admin.js
+++ cs-kiosk-frontend/src/pages/Admin.js
@@ -20,7 +20,9 @@ import React, { useEffect, useState } from 'react';
 import RadioGroup from "@cloudscape-design/components/radio-group";
 import Input from "@cloudscape-design/components/input";
 import { getSlides } from "../utility/retrieve_slides";
-
+import Cards from "@cloudscape-design/components/cards";
+import Link from "@cloudscape-design/components/link";
+import Box from "@cloudscape-design/components/box";
 
 export default function Admin() {
 
@@ -28,8 +30,10 @@ export default function Admin() {
     const [SlideName, setSlideName] = useState();
     const [ErrorValue, setErrorValue] = React.useState("");
     const slides = getSlides();
+    const which = false;
 
-    return <div>
+    return which
+    ? <div>
         <Form
             actions={
                 <SpaceBetween direction="horizontal" size="xs">
@@ -68,6 +72,97 @@ export default function Admin() {
             </FormField>
         </Form>
     </div>
+    : <Cards
+        ariaLabels={{
+            itemSelectionLabel: (e, t) => `select ${t.name}`,
+            selectionGroupLabel: "Item selection"
+        }}
+        cardDefinition={{
+            header: item => (
+                <Link fontSize="heading-m">{item.name}</Link>
+            ),
+            sections: [
+                {
+                    id: "description",
+                    header: "Description",
+                    content: item => item.description
+                },
+                {
+                    id: "type",
+                    header: "Type",
+                    content: item => item.type
+                },
+                {
+                    id: "size",
+                    header: "Size",
+                    content: item => item.size
+                }
+            ]
+        }}
+        cardsPerRow={[
+            { cards: 1 },
+            { minWidth: 500, cards: 2 }
+        ]}
+        items={[
+            {
+                name: "Item 1",
+                alt: "First",
+                description: "This is the first item",
+                type: "1A",
+                size: "Small"
+            },
+            {
+                name: "Item 2",
+                alt: "Second",
+                description: "This is the second item",
+                type: "1B",
+                size: "Large"
+            },
+            {
+                name: "Item 3",
+                alt: "Third",
+                description: "This is the third item",
+                type: "1A",
+                size: "Large"
+            },
+            {
+                name: "Item 4",
+                alt: "Fourth",
+                description: "This is the fourth item",
+                type: "2A",
+                size: "Small"
+            },
+            {
+                name: "Item 5",
+                alt: "Fifth",
+                description: "This is the fifth item",
+                type: "2A",
+                size: "Large"
+            },
+            {
+                name: "Item 6",
+                alt: "Sixth",
+                description: "This is the sixth item",
+                type: "1A",
+                size: "Small"
+            }
+        ]}
+        loadingText="Loading resources"
+        empty={
+            <Box textAlign="center" color="inherit">
+                <b>No resources</b>
+                <Box
+                    padding={{ bottom: "s" }}
+                    variant="p"
+                    color="inherit"
+                >
+                    No resources to display.
+                </Box>
+                <Button>Create resource</Button>
+            </Box>
+        }
+        header={<Header>Example Cards</Header>}
+    />;
 }
 
 function deleteSlide() {
