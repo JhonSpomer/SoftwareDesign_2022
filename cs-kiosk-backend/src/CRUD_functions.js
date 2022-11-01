e8064b5e243949366a949d2a429732291278cc10
Byte-By-Byte <00isaac.vh@gmail.com>
2022-11-01 09:19:22 -0600

    implementing generic slide functions

--- cs-kiosk-backend/src/CRUD_functions.js
+++ cs-kiosk-backend/src/CRUD_functions.js
@@ -121,8 +121,80 @@ module.exports = {
         }
     },
 
+    updSlide: async function (_slideName, _slideType, _user, _date, _expDate, targetID) {
+        await client.connect();
+        try {
+            //if no existing document ID is provided, create a new slide record.
+            if (targetID === undefined) {            
+                // slide metadata document
+                const slideDoc =
+                {
+                    slide_name: _slideName,
+                    slide_type: _slideType,
+                    owner: _user,
+                    lastModifiedBy: _user,
+                    expiration_date: _expDate
+                };
+                const result = await users.updateOne({slideDoc}, { upsert: true });
+                console.log(`A document was updated with the _id: ${result.upsertedId}`);
+            }
+            else if (targetID !== undefined) 
+            {
+                                // slide metadata document
+                                const slideDoc =
+                                {
+                                    slide_name: _slideName,
+                                    slide_type: _slideType,
+                                    // an existing document should already have an owner.
+                                    lastModifiedBy: _user,
+                                    expiration_date: _expDate
+                                };
+                                const result = await users.updateOne({slideDoc}, { upsert: true });
+                                console.log(`A document was updated with the _id: ${result.upsertedId}`);
+            }
+            else{
 
-    delSlide: async function (_targetID) {
+            }
+            //update document with given username
+            //upsert set to true - will insert given document if it does not already exixst
+            const result = await users.updateOne({ username: oldUN, }, { $set: upDoc }, { upsert: true });
+            console.log(`A document was updated with the _id: ${result.upsertedId}`);
+        }
+        finally {
+            // await client.close();
+        }
+    },
+
+
+    // delSlide: async function (_UN) {
+    //     await client.connect();
+    //     try {
+    //         //delete document with given username
+    //         const result = await users.deleteOne({ username: _UN });
+    //         console.log(`${result.deletedCount} document(s) deleted.`);
+    //     }
+    //     finally {
+    //         // await client.close();
+    //     }
+    // },
+
+    // getSlide: async function (UN, PS) {
+    //     await client.connect();
+    //     //how are we handling checking users against the user DB?
+    //     //return raw results from DB
+    //     var user;
+    //     try 
+    //     {
+    //         user = users.findOne({ username:UN, password:PS }, {username:1, password:1});
+    //     }
+    //     finally 
+    //     {
+    //         return user;
+    //     }
+ 
+    // },
+
+    delFile: async function (_targetID) {
         await client.connect();
         try {
             //delete document with given uID
