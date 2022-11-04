f7b8edd0e3bb3a1983161bcdea536243e0db9306
Matthew-MT <thumpber540@gmail.com>
2022-11-03 20:55:16 -0600

    Try again

--- cs-kiosk-backend/src/CRUD_functions.js
+++ cs-kiosk-backend/src/CRUD_functions.js
@@ -135,30 +135,24 @@ module.exports = {
                     lastModifiedBy: _user,
                     expiration_date: _expDate
                 };
-                const result = await users.updateOne({slideDoc}, { upsert: true });
+                const result = await slides.updateOne({}, {$set: {slideDoc}}, { upsert: true });
                 console.log(`A document was updated with the _id: ${result.upsertedId}`);
             }
-            else if (targetID !== undefined) 
+            else
             {
-                                // slide metadata document
-                                const slideDoc =
-                                {
-                                    slide_name: _slideName,
-                                    slide_type: _slideType,
-                                    // an existing document should already have an owner.
-                                    lastModifiedBy: _user,
-                                    expiration_date: _expDate
-                                };
-                                const result = await users.updateOne({slideDoc}, { upsert: true });
-                                console.log(`A document was updated with the _id: ${result.upsertedId}`);
+                // slide metadata document
+                const slideDoc =
+                {
+                    slide_name: _slideName,
+                    slide_type: _slideType,
+                    // an existing document should already have an owner.
+                    lastModifiedBy: _user,
+                    expiration_date: _expDate
+                };
+                const result = await slides.updateOne({targetID}, {$set: {slideDoc}}, {upsert: true});
+                console.log(`A document was updated with the _id: ${result.upsertedId}`);
             }
-            else{
-
-            }
-            //update document with given username
-            //upsert set to true - will insert given document if it does not already exixst
-            const result = await users.updateOne({ username: oldUN, }, { $set: upDoc }, { upsert: true });
-            console.log(`A document was updated with the _id: ${result.upsertedId}`);
+              console.log(`A document was updated with the _id: ${result.upsertedId}`);
         }
         finally {
             // await client.close();
@@ -166,33 +160,30 @@ module.exports = {
     },
 
 
-    // delSlide: async function (_UN) {
-    //     await client.connect();
-    //     try {
-    //         //delete document with given username
-    //         const result = await users.deleteOne({ username: _UN });
-    //         console.log(`${result.deletedCount} document(s) deleted.`);
-    //     }
-    //     finally {
-    //         // await client.close();
-    //     }
-    // },
+    delSlide: async function (_targetID) {
+        await client.connect();
+        try {
+            //delete document with given uID
+            const result = await slides.deleteOne({ upsertedId: _targetID });
+            console.log(`${result.deletedCount} document(s) deleted.`);
+        }
+        finally {
+            // await client.close();
+        }
+    },
 
-    // getSlide: async function (UN, PS) {
-    //     await client.connect();
-    //     //how are we handling checking users against the user DB?
-    //     //return raw results from DB
-    //     var user;
-    //     try 
-    //     {
-    //         user = users.findOne({ username:UN, password:PS }, {username:1, password:1});
-    //     }
-    //     finally 
-    //     {
-    //         return user;
-    //     }
- 
-    // },
+    getSlide: async function (targetID) {
+        await client.connect();
+        var slide;
+        try 
+        {
+            slide = slides.findOne({ username:UN, password:PS }, {username:1, password:1});
+        }
+        finally 
+        {
+            return slide;
+        }
+    },
 
     delFile: async function (_targetID) {
         await client.connect();
