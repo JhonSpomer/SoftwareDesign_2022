80eeec66a3eda2342b0e1783dd23e77106317abc
Matthew-MT <thumpber540@gmail.com>
2022-11-03 19:29:01 -0600

    Here

--- cs-kiosk-backend/main.js
+++ cs-kiosk-backend/main.js
@@ -114,7 +114,7 @@ const
         req.on("close", async () => {
             console.log("Closed");
             const slide = JSON.parse(buffer);
-            const id = await db.updSlide(slide.name, slide.type, undefined, undefined, undefined, req.params.id);
+            const id = await db.updSlide(slide.name, slide.type, undefined, undefined, undefined, slide.content, req.params.id);
             for (const ws of Object.values(connections)) ws.send("update");
             res
                 .status(200)
