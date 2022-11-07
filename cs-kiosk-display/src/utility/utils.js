5b39b706fe0da4a6eedcf18281d4a5f41193db60
Matthew-MT <thumpber540@gmail.com>
2022-11-06 18:51:14 -0700

    Working on getting images to load

--- cs-kiosk-display/src/utility/utils.js
+++ cs-kiosk-display/src/utility/utils.js
@@ -1,9 +1,13 @@
 export function ImgFromArrayBuffer(props) {
     let
         arrayBuffer = props.arrayBuffer,
-        mimeType = props.MIMEtype;
+        mimeType = props.mimeType;
+    console.log(arrayBuffer);
+    console.log(mimeType);
     delete props["arrayBuffer"];
     delete props["mimeType"];
+    let blob = new Blob([new Uint8Array(arrayBuffer)], {type: mimeType});
+    console.log(blob);
     return <img
         src={window.URL.createObjectURL(new Blob([new Uint8Array(arrayBuffer)], {type: mimeType}))}
         {...props}
