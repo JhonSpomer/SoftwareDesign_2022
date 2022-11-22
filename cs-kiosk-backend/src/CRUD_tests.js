1900902f31c355983a8a6e160b7275e1c96012a5
Byte-By-Byte <00isaac.vh@gmail.com>
2022-11-22 12:58:37 -0700

    outlined tests for CRUD functions

---
+++ cs-kiosk-backend/src/CRUD_tests.js
@@ -0,0 +1,22 @@
+const db = require("./src/CRUD_functions")
+
+function main()
+{
+    /*
+        check for user
+        create user
+        check for user
+        mod user
+        check for user
+        retireve user obj
+        del user
+        check for user
+        get slide
+        add slide
+        get slide
+        mod slide
+        get slide
+        del slide 
+        get slide
+    */
+}
